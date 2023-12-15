410ae0995f5432e7a4432654ff875a54698a4d53
Alberto <alberto.moreno.sn@gmail.com>
2023-12-15 09:50:48 +0000

    in process

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const nextButton = document.getElementById("nextButton");
 let currentIndex = 0;
 
 function changeImage(offset) {
-  currentIndex += offset;
-  if (currentIndex < 0) {
-    currentIndex = gallery.length - 1;
-  } else if (currentIndex >= gallery.length) {
-    currentIndex = 0;
-  }
-  updateDisplayImage();
+    currentIndex += offset;
+    if (currentIndex < 0) {
+        currentIndex = gallery.length - 1;
+    } else if (currentIndex >= gallery.length) {
+        currentIndex = 0;
+    }
+    updateDisplayImage();
 }
 
 function updateDisplayImage() {
-  const selectedImage = gallery[currentIndex];
-  displayImage.src = selectedImage.src;
-  displayImage.alt = selectedImage.alt;
+    const selectedImage = gallery[currentIndex];
+    displayImage.src = selectedImage.src;
+    displayImage.alt = selectedImage.alt;
 
-  displayImage.focus(); // Focus on the newly displayed image
-  console.log(`Image focused: ${selectedImage.alt}`); // Log to the console
+    displayImage.focus(); // Focus on the newly displayed image
+    console.log(`Image focused: ${selectedImage.alt}`); // Log to the console
 }
 
 prevButton.addEventListener("click", () => changeImage(-1));
@@ -29,36 +29,17 @@ nextButton.addEventListener("click", () => changeImage(1));
 
 // Keyboard navigation
 document.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowLeft") {
-    changeImage(-1);
-  } else if (event.key === "ArrowRight") {
-    changeImage(1);
-  }
+    if (event.key === "ArrowLeft") {
+        changeImage(-1);
+    } else if (event.key === "ArrowRight") {
+        changeImage(1);
+    }
 });
 
 // Add focus event for each image in the gallery
 gallery.forEach((thumb, index) => {
-  thumb.addEventListener("focus", () => {
-    currentIndex = index;
-    updateDisplayImage();
-  });
+    thumb.addEventListener("focus", () => {
+        currentIndex = index;
+        updateDisplayImage();
+    });
 });
-
-
-/*
-
-
-
-// add an event lister for each just to console log when it is being focused by the user
-for (let i = 0; i < elements.length; i++) {
-  elements[i].addEventListener("focus", function (event) {
-    console.log(`Element ${event.currentTarget} is focused`);
-  });
-}
-
-
-const elements = document.querySelectorAll("input, button, textarea, a"); // returns an array of DOM nodes
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-  console.log("Button has been clicked");
-});*/
